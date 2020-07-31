dfa143cf18d59b44f8d2c7a7d1119cdc5b463faa
skhabiri <shahnam.khabiri@gmail.com>
2020-07-30 17:08:58 -0700

    adding images

--- app/page/index.py
+++ app/page/index.py
@@ -7,7 +7,7 @@ import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-import plotly.express as px
+# import plotly.express as px
 
 
 # 2 column layout. 1st column width = 4/12
@@ -16,38 +16,20 @@ column1 = dbc.Col(
     [
         dcc.Markdown(
             """
-        
-            ## Your Value Proposition
+five different machine learning classifiers are implemented to classify a multi-class target label. The classification techniques are evaluated on the forest cover type dataset provided by Jock A. Blackard and Colorado State University.
 
-            Emphasize how the app will benefit users. Don't emphasize the underlying technology.
-
-            ✅ Test Heroku is a running app that adapts to your fitness levels and designs personalized workouts to help you 
-            improve your running. 
-
-            ❌ Test Heroku is the only intelligent running app that uses sophisticated deep neural net machine learning to 
-            make your run smarter because we believe in ML driven workouts. 
-
+> Bache, K. & Lichman, M. (2013). UCI Machine Learning Repository. Irvine, CA: University of California, School of Information and Computer Science
             """
         ),
         dcc.Link(
-            dbc.Button("Your Call To Action", color="primary"), href="/predictions"
+            dbc.Button("Evaluation Metrics", color="primary"), href="/predictions"
         ),
     ],
     md=4,
 )
 
-gapminder = px.data.gapminder()
-fig = px.scatter(
-    gapminder.query("year==2007"),
-    x="gdpPercap",
-    y="lifeExp",
-    size="pop",
-    color="continent",
-    hover_name="country",
-    log_x=True,
-    size_max=60,
-)
 
-column2 = dbc.Col([dcc.Graph(figure=fig),])
+column2 = dbc.Col([dbc.Row(html.Img(src= 'post2_pairplot2.png',style={'height': '50%','width': '50%'})),
+                   dbc.Row(html.Img(src= 'post2_countplot1.png',style={'height': '50%','width': '50%'}))])
 
 layout = dbc.Row([column1, column2])
